9cf68b5f7698e08ad1cfc221d75a02c7c1e5345f
Hetvi2512 <hetvishah2512@gmail.com>
2022-08-29 10:32:10 +0530

    Removed check body

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,14 +5,13 @@ const {
   getAllTours,
   getToursById,
   updateTour,
-  checkBody,
 } = require('../controllers/tourController');
 const router = express.Router();
 
 // router.param runs for certain parameter only
 //router.param('id', checkID)
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getToursById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
