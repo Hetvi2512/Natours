4a1b62269be82d9266201b36e22f450183bd5da4
Hetvi2512 <hetvishah2512@gmail.com>
2022-08-29 10:14:20 +0530

    removed static code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,20 +1,4 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel')
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-const checkID = (req, res, next, val) => {
-  // Middle Ware
-  console.log(`Tour id is: ${val}`);
- if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
 
 const checkBody = (req, res, next)=>{
   console.log("checkbody",req.body)
@@ -28,28 +12,13 @@ const checkBody = (req, res, next)=>{
   next();
 }
 const getAllTours = (req, res) => {
-  res.status(200).json({ status: 'success', results: tours?.length, tours });
+  // res.status(200).json({ status: 'success', results: tours?.length, tours });
 };
 const getToursById = (req, res) => {
-  const tour = tours.find((el) => el.id === +req.params.id);
-  res.status(200).json({ status: 'success', tour: tour ? tour : 'Invalid Id' });
+  // res.status(200).json({ status: 'success', tour: tour ? tour : 'Invalid Id' });
 };
 const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+ 
 };
 const updateTour = (req, res) => {
   res.status(200).json({
@@ -77,6 +46,5 @@ module.exports = {
   createTour,
   deleteTour,
   updateTour,
-  checkID,
   checkBody
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,15 +5,14 @@ const {
   getAllTours,
   getToursById,
   updateTour,
-  checkID,
   checkBody,
 } = require('../controllers/tourController');
 const router = express.Router();
 
 // router.param runs for certain parameter only
-router.param('id', checkID)
+//router.param('id', checkID)
 
-router.route('/').get(getAllTours).post(checkBody ,createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
 router.route('/:id').get(getToursById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
