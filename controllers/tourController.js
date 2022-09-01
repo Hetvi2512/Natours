85d4177ed5e883815895c21208c258b6cb5e3972
Hetvi2512 <hetvishah2512@gmail.com>
2022-09-01 17:38:30 +0530

    Refactored code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,64 +1,20 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 const aliasTopTours = async (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
+
 const getAllTours = async (req, res) => {
   try {
-    // Option-1
-    // 1-a) FIltering
-    const queryObj = { ...req?.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    // 1-b) Advanced filtering
-    // {difficulty:'easy', duration :{$gte:5}} - what mongoose wants
-    // {difficulty:'easy', duration :{gte:'5'}} - what we get through postman
-    // so replace gte,gt,lte,lt with$
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-    //Option-2
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .lte(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    // 2) Sorting
-    if (req?.query?.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      // {duration,price} - what we get from postman
-      // { duration price} - what mongo wants
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      // {name,duration,price} - what we get from postman
-      // {name duration price} - what mongo wants
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination * will convert string into number
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
-    const tours = await query;
-
+    const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+    const tours = await features.query;
     res.status(200).json({ status: 'success', results: tours?.length, tours });
   } catch (err) {
     console.log(err);
---
+++ utils/apiFeatures.js
@@ -0,0 +1,61 @@
+class APIFeatures {
+    constructor(query, queryString) {
+      this.query = query;
+      this.queryString = queryString;
+    }
+  
+    filter() {
+      const queryObj = { ...this.queryString };
+      const excludedFields = ['page', 'sort', 'limit', 'fields'];
+      excludedFields.forEach((el) => delete queryObj[el]);
+  
+      // 1B) Advanced filtering
+      // {difficulty:'easy', duration :{$gte:5}} - what mongoose wants
+      // {difficulty:'easy', duration :{gte:'5'}} - what we get through postman
+      // so replace gte,gt,lte,lt with$
+      let queryStr = JSON.stringify(queryObj);
+      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  
+      this.query = this.query.find(JSON.parse(queryStr));
+  
+      return this;
+    }
+  
+    sort() {
+      if (this.queryString.sort) {
+        // {duration,price} - what we get from postman
+        // { duration price} - what mongo wants
+        const sortBy = this.queryString.sort.split(',').join(' ');
+        this.query = this.query.sort(sortBy);
+      } else {
+        this.query = this.query.sort('-createdAt');
+      }
+  
+      return this;
+    }
+  
+    limitFields() {
+      if (this.queryString.fields) {
+        // {name,duration,price} - what we get from postman
+        // {name duration price} - what mongo wants
+        const fields = this.queryString.fields.split(',').join(' ');
+        this.query = this.query.select(fields);
+      } else {
+        this.query = this.query.select('-__v');
+      }
+  
+      return this;
+    }
+  
+    paginate() {
+      //* will convert string into number
+      const page = this.queryString.page * 1 || 1;
+      const limit = this.queryString.limit * 1 || 100;
+      const skip = (page - 1) * limit;
+  
+      this.query = this.query.skip(skip).limit(limit);
+  
+      return this;
+    }
+  }
+  module.exports = APIFeatures
