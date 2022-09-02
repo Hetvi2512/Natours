ca23d3450ef3407aa2a6dfb9b5a11cdc0d0a10ee
Hetvi2512 <hetvishah2512@gmail.com>
2022-09-02 16:47:11 +0530

    Added more validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,8 @@ const toursSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      maxlength: [10, 'A tour name must have more or equal then 10 characters'],
     },
     slug: String,
     duration: {
@@ -20,10 +22,16 @@ const toursSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty level specified'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy,medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 3.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
