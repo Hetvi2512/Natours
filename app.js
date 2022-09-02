a130242a42ba23dd940229f6d7dccbaf011302fb
Hetvi2512 <hetvishah2512@gmail.com>
2022-09-02 18:19:59 +0530

    added all routes

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status:'fail',
+        message:`Cannot find ${req.originalUrl}`
+    })
+})
 module.exports = app;
 
 // Option-1
